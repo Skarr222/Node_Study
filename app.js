f45dd048419562bde94601745d026245baf0d770
Oskar Gomza <oskargomza54@gmail.com>
2022-09-18 14:40:41 +0200

    feat(node): Test middlewares feture

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const express = require("express");
 
 const app = express();
 
+/*
+ * MiddLewares
+ * app.use(auth);
+ * app.use("/posts", () => {
+ *  console.log("This is middleware running");
+ * });
+ */
 //ROUTES
 app.get("/", (req, res) => {
   res.send("We are on home");
+});
+app.get("/posts", (req, res) => {
+  res.send("We are on post");
 });
 
 //How to start listening to the server
