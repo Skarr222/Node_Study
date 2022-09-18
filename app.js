6975f4b094e67a3b3fcfdec9f49e1790579631ea
Oskar Gomza <oskargomza54@gmail.com>
2022-09-18 14:21:36 +0200

    feat(node): Adding route to test start

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const express = require("express");
 const app = express();
 
 //ROUTES
-http.createServer;
+app.get("/", (req, res) => {
+  res.send("We are on home");
+});
+
+//How to start listening to the server
+app.listen(3000);
